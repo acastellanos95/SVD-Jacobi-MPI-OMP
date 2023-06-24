8c3a7fcedb0321ee505988d786f221e30c3c09a6
Andre Castellanos <47199821+acastellanos95@users.noreply.github.com>
2023-06-24 10:26:06 -0600

    all ready

---
+++ cmake-build-debug/run.sh
@@ -0,0 +1,6 @@
+mpiexec -n 3 -v -display-map -display-allocation -bind-to core ./SVD_Jacobi_MPI_OMP 5000
+mpiexec -n 3 -v -display-map -display-allocation -bind-to core ./SVD_Jacobi_MPI_OMP 10000
+mpiexec -n 3 -v -display-map -display-allocation -bind-to core ./SVD_Jacobi_MPI_OMP 15000
+mpiexec -n 3 -v -display-map -display-allocation -bind-to core ./SVD_Jacobi_MPI_OMP 20000
+mpiexec -n 3 -v -display-map -display-allocation -bind-to core ./SVD_Jacobi_MPI_OMP 30000
+mpiexec -n 3 -v -display-map -display-allocation -bind-to core ./SVD_Jacobi_MPI_OMP 50000
